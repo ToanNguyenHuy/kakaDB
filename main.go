6cf9f0896e42c080b9f233c5ac0128027099bbc1
ToanNguyenHuy <nguyenhuytoan002@gmail.com>
2025-09-16 15:13:03 +0700

    This code creates the file if it does not exist, or truncates the existing one before writing the content. And most importantly, the data is not persistent unless you call fsync (fp.Sync() in Go).

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
+	// File path to write
+	path := "output.txt"
+
 	for {
 		fmt.Print("kkDB >> ")
 		input, _ := reader.ReadString('\n')
@@ -19,7 +22,28 @@ func main() {
 			fmt.Println("Bye 👋")
 			break
 		}
-		fmt.Println("You typed:", input)
+
+		err := SaveData1(path, []byte(input))
+		if err != nil {
+			fmt.Println("Error saving data: ", err)
+		} else {
+			fmt.Println("Data saved successfully")
+		}
+
 	}
 
+}
+
+func SaveData1(path string, data []byte) error {
+	fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+
+	_, err = fp.Write(data)
+	if err != nil {
+		return err
+	}
+	return fp.Sync()
 }
